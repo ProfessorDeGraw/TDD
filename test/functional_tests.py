08d919799a6ac450586e2647e9eaaff844f63407
Nathan De Graw <nddegraw@mash.localdomain>
2017-12-12 07:09:45 -0500

    Extra cart

--- test/functional_tests.py
+++ test/functional_tests.py
@@ -1,5 +1,9 @@
+import time
 from pyvirtualdisplay import Display
 from selenium import webdriver
+import unittest
+
+from selenium.webdriver.common.keys import Keys
 
 
 def take_screen_shot(headless_browser):
@@ -15,30 +19,88 @@ def dump_html(headless_browser):
         f.write(headless_browser.page_source)
 
 
-display = Display(visible=0, size=(1024, 768))
-display.start()
+class NewVisitorTest(unittest.TestCase):
 
-options = webdriver.ChromeOptions()
-options.add_argument("--no-sandbox")
-#options.binary_location = "/usr/bin/google-chrome"
-print('set up driver')
-browser = webdriver.Chrome(chrome_options=options)
-#driver= webdriver.Firefox()
-print('staring driver')
+    def setUp(self):
+        self.display = Display(visible=0, size=(1024, 768))
+        self.display.start()
 
-browser.set_window_size(1024, 768)
+        options = webdriver.ChromeOptions()
+        options.add_argument("--no-sandbox")
+        # options.binary_location = "/usr/bin/google-chrome"
+        print('set up driver')
+        self.browser = webdriver.Chrome(chrome_options=options)
+        # self.browser = webdriver.Firefox()
 
-# > Xvfb :99 -ac -screen 0 1280x1024x24 &
-# > export DISPLAY=:99
-# > java -jar selenium-server-standalone-3.6.0.jar
+        self.browser.set_window_size(1024, 768)
 
-browser.get('http://localhost:8000')
+        # How to run in command window
+        # > Xvfb :99 -ac -screen 0 1280x1024x24 &
+        # > export DISPLAY=:99
+        # > java -jar selenium-server-standalone-3.6.0.jar
 
-take_screen_shot(browser)
-dump_html(browser)
+    def tearDown(self):
+        # driver.close() # Close the current window.
+        self.browser.quit()  # Quit the driver and close every associated window.
+        self.display.stop()
 
-assert 'Django' in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
 
-# driver.close() # Close the current window.
-browser.quit() # Quit the driver and close every associated window.
-display.stop()
+        take_screen_shot(self.browser)
+        dump_html(self.browser)
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+        self.fail('Finish the test!')
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
